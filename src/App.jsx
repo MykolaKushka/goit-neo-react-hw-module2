89996f3bfdefa2aaaad93e9eaf55d3aae58890f8
Mykola Kushka <Mykola.Kushka@gmail.com>
2025-01-12 20:19:52 +0200

    description in separated component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import { useState, useEffect } from 'react';
 import Feedback from './components/Feedback/Feedback';
 import Options from './components/Options/Options';
 import Notification from './components/Notification/Notification';
+import Description from './components/Description/Description';
 import styles from './App.module.css';
 
 const App = () => {
-  // Ініціалізація стану з localStorage або з нулями
   const [feedback, setFeedback] = useState(() => {
     const savedFeedback = localStorage.getItem('feedback');
     return savedFeedback ? JSON.parse(savedFeedback) : { good: 0, neutral: 0, bad: 0 };
   });
 
-  // Оновлення стану при натисканні на кнопки
   const updateFeedback = (feedbackType) => {
     setFeedback((prevFeedback) => ({
       ...prevFeedback,
@@ -19,7 +18,6 @@ const App = () => {
     }));
   };
 
-  // Скидання стану
   const resetFeedback = () => {
     setFeedback({
       good: 0,
@@ -28,25 +26,18 @@ const App = () => {
     });
   };
 
-  // Обчислення загальної кількості фідбеків
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
-  // Обчислення відсотка позитивних відгуків
   const positivePercentage =
     totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
 
-  // Використовуємо useEffect для збереження стану у localStorage
   useEffect(() => {
     localStorage.setItem('feedback', JSON.stringify(feedback));
   }, [feedback]);
 
   return (
     <div className={styles.app}>
-      <h1>Sip Happens Café</h1>
-      <p>
-        Please leave your feedback about our service by selecting one of the
-        options below.
-      </p>
+      <Description />
       <Options
         updateFeedback={updateFeedback}
         resetFeedback={resetFeedback}
